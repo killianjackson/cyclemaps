444e8f9430483dbc2671305447a1c5c08e7c814c
Killian Jackson <killianjacksn@gmail.com>
2021-01-31 22:18:35 -0800

    Initial Commit

---
+++ CycleMaps/CycleMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleMapsApp.swift
+//  CycleMaps
+//
+//  Created by Killian Jackson on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct CycleMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
